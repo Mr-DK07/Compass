4c19b0839914c511139bde159fd8e37895cea122
DK <ashukr8081@gmail.com>
2025-04-16 01:36:47 +0530

    Add new PNG images for public use
    - Added images1.png with a size of 7175 bytes.
    - Added images2.png with a size of 7351 bytes.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Logo({ width = "100px" }) {
-  return <div className={width}>Logo</div>;
+function Logo() {
+  return (
+    <div>
+      {/* <img src="../public/images2.png" alt="img1" 
+      className="w-[130px] h-[60px]"
+      /> */}
+
+      <h2 className="text-3xl font-bold text-orange">Compass</h2>
+    </div>
+  );
 }
 
 export default Logo;
